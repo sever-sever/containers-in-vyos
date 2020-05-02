e10ac59896577205fdc297739dcd1683d719021b
sever <seversss265@gmail.com>
2020-05-02 17:36:31 +0300

    Automate network

--- create-lxc.sh
+++ create-lxc.sh
@@ -15,8 +15,16 @@ sudo apt-get -y install lxc xz-utils
 # Delete temp apt sources
 sudo rm -f /etc/apt/sources.list.d/mysource.list
 
-# Add bridge with ip-address to communicate with lxc container
-sudo brctl addbr $BR_NAME
+# Check if bridge is exists
+sudo brctl show $BR_NAME > /dev/null
+
+if [ $? -eq 1 ]; then
+    # Add bridge
+    sudo brctl addbr $BR_NAME
+else
+    echo "bridge [$BR_NAME] is exists."
+fi
+
 sudo ifconfig $BR_NAME $BR_IP
 
 sudo mkdir -p /etc/lxc/.config/lxc
@@ -29,10 +37,12 @@ lxc.net.0.flags = up
 lxc.net.0.name = eth1
 lxc.net.0.ipv4.address = 10.0.3.2/24 10.0.3.255
 lxc.net.0.ipv4.gateway = 10.0.3.1
+lxc.uts.name = alpine
 lxc.apparmor.profile = generated
 lxc.apparmor.allow_nesting = 1
 lxc.idmap = u 0 100000 65536
 lxc.idmap = g 0 100000 65536
+#lxc.start.auto = 1   # Autostart
 EOF
 "
 # Create lxc container "Alpine"
@@ -40,5 +50,4 @@ sudo lxc-create -f /etc/lxc/.config/lxc/default.conf --template download --name 
 
 # Start lxc container
 sudo lxc-start -n $CONTAINER_NAME -d
-
 
